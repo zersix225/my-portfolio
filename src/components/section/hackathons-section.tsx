54073483f88b113b00512e5cd7dcc903986396b6
zersix225 <drago.overlond@gmail.com>
2026-07-03 17:19:11 +0700

    chore: fix at hacathon section

--- src/components/section/hackathons-section.tsx
+++ src/components/section/hackathons-section.tsx
@@ -53,23 +53,23 @@ export default function HackathonsSection() {
                     {hackathon.description}
                   </p>
                 )}
-                {hackathon.links && hackathon.links.length > 0 && (
-                  <div className="mt-1 flex flex-row flex-wrap items-start gap-2">
-                    {hackathon.links.map((link, idx) => (
-                      <Link
-                        href={link.href}
-                        key={idx}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Badge className="flex items-center gap-1.5 text-xs bg-primary text-primary-foreground">
-                          {link.icon}
-                          {link.title}
-                        </Badge>
-                      </Link>
-                    ))}
-                  </div>
-                )}
+                {/*{hackathon.links && hackathon.links.length > 0 && (*/}
+                {/*  <div className="mt-1 flex flex-row flex-wrap items-start gap-2">*/}
+                {/*    {hackathon.links.map((link, idx) => (*/}
+                {/*      <Link*/}
+                {/*        href={link.href}*/}
+                {/*        key={idx}*/}
+                {/*        target="_blank"*/}
+                {/*        rel="noopener noreferrer"*/}
+                {/*      >*/}
+                {/*        <Badge className="flex items-center gap-1.5 text-xs bg-primary text-primary-foreground">*/}
+                {/*          {link.icon}*/}
+                {/*          {link.title}*/}
+                {/*        </Badge>*/}
+                {/*      </Link>*/}
+                {/*    ))}*/}
+                {/*  </div>*/}
+                {/*)}*/}
               </div>
             </TimelineItem>
           ))}
